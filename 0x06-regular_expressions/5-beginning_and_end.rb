ee785d483e64db8dce21fba5194d3f2a7b91baa1
Karilaw <lawrencekariithi6@gmail.com>
2023-08-01 04:02:23 -0700

    first commit to test

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
 
 if ARGV.length != 1
-  puts "Usage: ./4-repetition_token_3.rb <string>"
+  puts "Usage: ./5-beginning_and_end.rb <string>"
   exit
 end
 
 string = ARGV[0]
-regex = /hbt*n/
+regex = /\Ah.n\z/
 matches = string.scan(regex)
 puts matches.join
