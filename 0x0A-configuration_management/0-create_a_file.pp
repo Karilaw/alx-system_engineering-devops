24ef6a34772b2e89d05bac13bb9d6d03b5f0633b
Karilaw <lawrencekariithi6@gmail.com>
2023-09-22 00:52:30 -0700

    committing task 0 to test

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# creates a file at /tmp/school with specific owner, group, permissions
+# and contents
+
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
