60b6358a9a7121a7122cfa4c626cc2561f0601b7
Karilaw <lawrencekariithi6@gmail.com>
2023-09-27 03:05:48 -0700

    corrections tasks

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,30 +1,10 @@
 # Puppet manifest to configure SSH client
-
-class ssh_client_config {
-  file { '/etc/ssh/ssh_config':
-    ensure  => present,
-    content => template('ssh/ssh_config.erb'),
-  }
+file_line { 'Declare identity file':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
 }
 
-class ssh_disable_password_auth {
-  file_line { 'Disable Password Authentication':
-    path => '/etc/ssh/ssh_config',
-    line => 'PasswordAuthentication no',
-  }
+file_line { 'Turn off passwd auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
 }
-
-class ssh_set_identity_file {
-  file_line { 'Set Identity File':
-    path => '/etc/ssh/ssh_config',
-    line => 'IdentityFile ~/.ssh/school',
-  }
-}
-
-class ssh {
-  include ssh_client_config
-  include ssh_disable_password_auth
-  include ssh_set_identity_file
-}
-
-include ssh
