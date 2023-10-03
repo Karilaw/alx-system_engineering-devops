a27531bc906814cfaf5b7ef4ab94f79fa18acf87
Karilaw <lawrencekariithi6@gmail.com>
2023-10-03 08:47:11 -0700

    more more corrections

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,27 +1,37 @@
 # This Puppet manifest installs Nginx and configures it to add a custom header 'X-Served-By' with the value of the hostname of the server.
 
-class nginx_custom_header {
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  file { '/etc/nginx/snippets/custom-header.conf':
-    ensure  => file,
-    content => "add_header X-Served-By \$hostname;",
-    require => Package['nginx'],
-  }
-
-  exec { 'nginx_include_custom_header':
-    command => "sed -i '/server_name _;/a include snippets/custom-header.conf;' /etc/nginx/sites-available/default",
-    path    => ['/bin', '/usr/bin'],
-    require => File['/etc/nginx/snippets/custom-header.conf'],
-  }
-
-  service { 'nginx':
-    ensure    => running,
-    enable    => true,
-    subscribe => Exec['nginx_include_custom_header'],
-  }
+# Ensure nginx is installed
+package { 'nginx':
+  ensure => installed,
 }
 
-include nginx_custom_header
+# Ensure nginx service is running
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
+  require    => Package['nginx'],
+}
+
+# Configure nginx
+file_line { 'listen 80':
+  path => '/etc/nginx/sites-available/default',
+  line => '    listen 80;',
+  match => '^\s*listen 80 default_server;',
+  require => Package['nginx'],
+}
+
+# Set up the Hello World page
+file { '/var/www/html/index.nginx-debian.html':
+  ensure  => file,
+  content => "Hello World!\n",
+  require => Package['nginx'],
+}
+
+# Add the custom header to all location blocks in the default site configuration
+exec { 'add custom header':
+  command => "sudo sed -i \"/location /a\\\\\\\\tadd_header X-Served-By \$HOSTNAME;\" /etc/nginx/sites-available/default",
+  path    => '/usr/bin:/bin:/usr/sbin:/sbin',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
