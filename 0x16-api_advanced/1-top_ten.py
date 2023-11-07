229f2deb91b63854bd5d8e650e005ef4e0014ae1
Karilaw <lawrencekariithi6@gmail.com>
2023-11-07 10:19:19 -0800

    first commit to test

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Top Ten """
+import requests
+
+def top_ten(subreddit):
+    """
+    Query the Reddit API for the titles
+
+    Parameters:
+    subreddit (str): The name of the subreddit
+    """
+    headers = {'User-Agent': 'custom'}
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print(None)
+        return
+    data = response.json()['data']['children']
+    for post in data:
+        print(post['data']['title'])
